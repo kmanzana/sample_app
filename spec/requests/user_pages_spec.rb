0b190c23f9ab899a9d3276be6d22e9fbcdbc4ce0
Kelton Manzanares <kmanzana@mines.edu>
2013-01-12 00:56:15 -0700

    Refactored tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,16 +5,16 @@ describe "UserPages" do
 
   describe "signup page" do
     before { visit signup_path }
-    it { should have_selector('h1', text: 'Sign up') }
-    it { should have_selector('title', text: 'Sign up') }
+    it { should have_h1('Sign up') }
+    it { should have_title('Sign up') }
   end
 
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
 
-    it { should have_selector('h1',    text: user.name) }
-    it { should have_selector('title', text: user.name) }
+    it { should have_h1(user.name) }
+    it { should have_title(user.name) }
   end
 
   describe "signup" do
@@ -31,7 +31,7 @@ describe "UserPages" do
       describe "after submission" do
         before { click_button submit }
 
-        it { should have_selector('title', text: 'Sign up') }
+        it { should have_title('Sign up') }
         it { should have_content('error') }
       end
     end
@@ -48,8 +48,8 @@ describe "UserPages" do
         before { click_button submit }
         let(:user) { User.find_by_email('user@example.com') }
 
-        it { should have_selector('title', text: user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_title(user.name) }
+        it { should have_success_message('Welcome') }
         it { should have_link('Sign out') }
       end
 
